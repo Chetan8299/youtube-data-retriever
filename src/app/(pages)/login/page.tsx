9269ccc6512c82afa5068c84670b7f3c4467b6b5
Chetan Singh <csingh8299@gmail.com>
2025-01-10 05:16:31 +0530

    logout button

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,27 +1,40 @@
 "use client";
-import { signIn, useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { signIn,  , signOut,  , useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const Login = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
-  const handleGoogleSignIn = async () => {
-    try {
-      await signIn('google'); 
-      if(session) {
-        router.push("/home");
+    const router = useRouter();
+    const { data: session } = useSession();
+    const handleGoogleSignIn = async () => {
+        try {
+            await signIn("google");
+            if (session) {
+                router.push("/home");
+            }
+        } catch (error) {
+            console.error("Error during sign-in:", error);
+        }
+    };
+   
+    const handleSignOut = async () => {
+      try {
+          await signOut({ callbackUrl: "/" });
+      } catch (error) {
+          console.error("Error during sign-out:", error);
       }
-    } catch (error) {
-      console.error('Error during sign-in:', error);
-    }
   };
 
-  return (
-    <div>
-      <h1>Welcome to the App</h1>
-      <button onClick={handleGoogleSignIn}>Sign in with Google</button>
-    </div>
-  );
+
+    return session ? (
+        <div>
+            <button onClick={handleSignOut}>Log Out</button>
+        </div>
+    ) : (
+        <div>
+            <h1>Welcome to the App</h1>
+            <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+        </div>
+    );
 };
 
 export default Login;

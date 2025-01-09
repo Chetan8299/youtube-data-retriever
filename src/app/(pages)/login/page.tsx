b0e4424450f89c7dc1158b3e2a7a710e2d148d08
Chetan Singh <csingh8299@gmail.com>
2025-01-10 05:18:09 +0530

    minor fix

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { signIn,  , signOut,  , useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 const Login = () => {
